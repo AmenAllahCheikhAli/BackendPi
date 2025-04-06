e6c8029b1c4e8dbe36c8e00f8335eddc4b7e7054
AmenAllahCheikhAli <amenallah.cheikhali@esprit.tn>
2025-04-06 17:21:30 +0100

    Crud Reclamation & Don

---
+++ src/main/java/com/esprit/amen/entity/Don.java
@@ -0,0 +1,29 @@
+package com.esprit.amen.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@DynamicUpdate
+public class Don {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titre;
+    private String description;
+    private Double montant;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    @OneToMany(mappedBy = "don", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JsonIgnore
+    private List<Reclamation> reclamations;
+}
